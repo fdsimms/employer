76ea595d2d4e3e7ae8a9ad6184a3e5428d7d0a57
Frankie <fdsimms@gmail.com>
2016-05-14 12:53:31 -0400

    Continue styling modal.

--- js/components/modal.js
+++ js/components/modal.js
@@ -4,7 +4,15 @@ const Modal = ({ employee, isShowing }) => (
   isShowing ? (
     <div className="modal">
       <div className="modal-screen"></div>
-      <div className="modal-main"></div>
+      <div className="modal-main">
+        <div className="modal-image">
+          <img src={employee.imageUrl} />
+        </div>
+        <p className="modal-blurb">{employee.name} is a really important part of our team. We
+        think {employee.name} is a {employee.job}, but to be honest we
+        aren't positive what that means.
+        </p>
+      </div>
     </div>
   ) : null
 );
